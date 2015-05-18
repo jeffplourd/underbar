4e32cdf4d46d6ba9bc2493cd5527e71c00a2eee4
jplourd <jeffplourd@gmail.com>
2015-05-18 09:55:12 -0700

    Added 'extend' function that merges objects into other objects.

--- src/underbar.js
+++ src/underbar.js
@@ -298,6 +298,13 @@
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
+    for(var i = 1, len = arguments.length; i < len; i++) {
+      var objToAdd = arguments[i];
+      for(var val in objToAdd) {
+        obj[val] = objToAdd[val];
+      }
+    }
+    return obj;
   };
 
   // Like extend, but doesn't ever overwrite a key that already
